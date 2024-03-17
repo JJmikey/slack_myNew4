13622981a17db464d546854545039b84f6ef062f
JJmikey <144325604+JJmikey@users.noreply.github.com>
2024-03-17 16:14:27 +0800

    Add files via upload

--- slack_mynew4.py
+++ slack_mynew4.py
@@ -1,9 +1,12 @@
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 
-client = WebClient(token="你的Bot User OAuth Token")
+import os 
+
+token = os.getenv("Bot_User_OAuth_Token")
+client = WebClient(token=token)
 
 try:
     response = client.chat_postMessage(channel="#channel-name", text="Hello, world!")
 except SlackApiError as e:
-    print(f"Error: {e.response['error']}")
+    print(f"Error: {e.response['error']}")  
