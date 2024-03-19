648dbdd2f83b6bc5e327e2369012a694a07bf422
JJmikey <144325604+JJmikey@users.noreply.github.com>
2024-03-19 16:52:47 +0800

    Add files via upload

--- slack_mynew4.py
+++ slack_mynew4.py
@@ -1,15 +1,41 @@
 from flask import Flask, request, jsonify
 from slack_sdk.oauth import AuthorizeUrlGenerator
-from slack_sdk.web import WebClient
+from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 
 import os 
 
 app = Flask(__name__)
 
+slack_bot_token = os.environ["SLACK_BOT_TOKEN"] # 用以調用 Slack API 
+client = WebClient(token=slack_bot_token)
+
+
 slack_client_id = os.environ["SLACK_CLIENT_ID"]
 slack_client_secret = os.environ["SLACK_CLIENT_SECRET"]
-slack_redirect_uri = os.environ.get("SLACK_REDIRECT_URL")  # if applicable
+
+@app.route("/slack/events", methods=["POST"])
+def slack_events():
+    payload = request.json
+    if "challenge" in payload:
+        return payload["challenge"]  # 返回所需要的`challenge`参数的值
+    else:
+        # 在這裡處理其它事件
+        event = payload.get("event", {})
+
+        # 當收到訊息時
+        if payload.get("type") == "event_callback" and event.get("type") == "message":
+            channel_id = event.get("channel")
+            user = event.get("user")
+
+            # 防止 bot 自己回應自己的訊息
+            if user == "Your Bot User ID":
+                return jsonify({})
+
+            client.chat_postMessage(channel=channel_id, text="我收到你的訊息了!")
+
+        return jsonify({})
+
 
 @app.route("/slack/auth", methods=["GET"])
 def slack_oauth():
@@ -38,4 +64,4 @@ def site_map():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080))) #for deploy on vercel
