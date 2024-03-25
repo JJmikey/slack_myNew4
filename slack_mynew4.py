bb9dda62c53d1726a94577f29296f7db5540f1f1
JJmikey <144325604+JJmikey@users.noreply.github.com>
2024-03-25 14:00:56 +0800

    Add files via upload

--- slack_mynew4.py
+++ slack_mynew4.py
@@ -87,7 +87,7 @@ def handle_image(file_content,prompt):
         {
             "role": "user",
             "content": [
-            {"type": "text", "text": "What’s in this image?"},
+            {"type": "text", "text": {prompt}},
             {
                 "type": "image_url",
                 "image_url": {
