19eabf5e8e37154f3829fd539a9a91a7694c73da
JJmikey <144325604+JJmikey@users.noreply.github.com>
2024-03-27 15:55:09 +0800

    Add files via upload

--- slack_mynew4.py
+++ slack_mynew4.py
@@ -178,14 +178,15 @@ def slack_events():
                     # 限制历史消息的数目，并将历史消息添加到列表
                     # 注意：我们将历史消息从最早的开始添加，以维持他们的顺序
                     # 最古老訊息: 用reversed(history['messages'][-10:])表達
-                    for msg in (history['messages'][0:1]):
+                    # 获取最新的一对消息并反转，使其按照时间顺序
+                    messages = history['messages'][0:2][::-1]
+
+                    for msg in messages:
                         role = 'assistant' if msg['user'] == 'U06QDBXQESE' else 'user'
-                        messages.append(
-                            {
-                                "role": role,
-                                "content": msg['text']
-                            }
-                        )
+                        content = {
+                            "role": role,
+                            "content": msg['text']
+                        }
 
                     # 添加用户的当前消息
                     messages.append(
